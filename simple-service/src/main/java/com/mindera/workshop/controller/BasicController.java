ebb80e3725530cc4fb4b0c86c5662fe48cf77e55
César Rodrigues <cesar.rodrigues@mindera.com>
2019-10-17 22:42:03 +0100

    Add endpoint to force s service shutdown

--- simple-service/src/main/java/com/mindera/workshop/controller/BasicController.java
+++ simple-service/src/main/java/com/mindera/workshop/controller/BasicController.java
@@ -1,16 +1,28 @@
 package com.mindera.workshop.controller;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping("/workshop")
 public class BasicController {
+    private boolean shutdown = Boolean.FALSE;
 
     @GetMapping("/simple")
-    public String basicEndpoint() {
-        return "Hello World";
+    public ResponseEntity<String> basicEndpoint() throws UnknownHostException {
+        return shutdown ? ResponseEntity.notFound().build() :
+                ResponseEntity.ok("Hello World -> " + Inet4Address.getLocalHost().getHostName());
+    }
+
+    @GetMapping("/shutdown")
+    public String shutdown() throws UnknownHostException {
+        shutdown = Boolean.TRUE;
+        return "Shutting down....";
     }
 }
