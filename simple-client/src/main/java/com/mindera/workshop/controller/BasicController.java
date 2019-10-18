2aa7d091207c70377c684771f50e531efa45f77a
César Rodrigues <cesar.rodrigues@mindera.com>
2019-10-18 22:17:42 +0100

    New service acting as a client for simple-service

---
+++ simple-client/src/main/java/com/mindera/workshop/controller/BasicController.java
@@ -0,0 +1,43 @@
+package com.mindera.workshop.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/workshop")
+public class BasicController {
+    private final RestTemplate client;
+    private boolean shutdown = Boolean.FALSE;
+
+    @Autowired
+    public BasicController() {
+        this.client = new RestTemplate();
+    }
+
+    @GetMapping("/client")
+    public ResponseEntity<String> basicEndpoint() {
+        ResponseEntity<String> response = client.getForEntity("http://service-endpoint:80/workshop/simple", String.class);
+
+        String content;
+        if (response.getStatusCode().is2xxSuccessful()) {
+            content = response.getBody();
+        } else {
+            content = "Wrong response! Err[" + response.getStatusCode().toString() + "]";
+        }
+
+
+        return shutdown ? ResponseEntity.notFound().build() :
+                ResponseEntity.ok(content);
+    }
+
+    @GetMapping("/shutdown")
+    public String shutdown() {
+        shutdown = Boolean.TRUE;
+        return "Shutting down....";
+    }
+}
