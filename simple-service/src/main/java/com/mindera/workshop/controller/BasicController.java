e4d446d881dd7b75588d63eced5a82c1115148e5
César Rodrigues <cesar.rodrigues@mindera.com>
2022-03-27 21:44:43 +0100

    Update project for deployment scope

--- simple-service/src/main/java/com/mindera/workshop/controller/BasicController.java
+++ simple-service/src/main/java/com/mindera/workshop/controller/BasicController.java
@@ -3,6 +3,7 @@ package com.mindera.workshop.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +15,18 @@ import java.net.UnknownHostException;
 public class BasicController {
     private boolean shutdown = Boolean.FALSE;
 
-    @GetMapping("/simple")
-    public ResponseEntity<String> basicEndpoint() throws UnknownHostException {
+    @GetMapping("/{team}/simple")
+    public ResponseEntity<String> basicEndpoint(@PathVariable("team") String team) throws UnknownHostException {
+        StringBuilder contents = new StringBuilder();
+        contents.append("Hello World -> ")
+                .append(Inet4Address.getLocalHost().getHostName())
+                .append("\n")
+                .append("Well done team workshop-")
+                .append(team)
+                .append("!\n");
+
         return shutdown ? ResponseEntity.notFound().build() :
-                ResponseEntity.ok("Hello World -> " + Inet4Address.getLocalHost().getHostName());
+                ResponseEntity.ok(contents.toString());
     }
 
     @GetMapping("/shutdown")
