8b8fe592bc055555aa43d58dc79f4f0092185ac1
César Rodrigues <cesar.rodrigues@mindera.com>
2022-03-27 21:43:45 +0100

    Update setup (Bat not working) to use a specific namespace on a cluster

--- setup.bat
+++ setup.bat
@@ -1,4 +1,6 @@
-kubectl config set-cluster workshop --insecure-skip-tls-verify=true --server=https://$1
-kubectl config set-credentials participant --username=admin --password=$2
-kubectl config set-context workshop --cluster=workshop --user=participant
+kubectl config set-cluster workshop --embed-certs --certificate-authority=workshop.ca.crt --server=https://34.65.48.127
+kubectl config set-credentials workshop --auth-provider=gcp
+export GOOGLE_APPLICATION_CREDENTIALS=workshop.json
+kubectl config set-context workshop --cluster=workshop --user=workshop --namespace=$1
 kubectl config use-context workshop
+kubectl get nodes
