e79628f6be72516edb88a7af3e0209cab89f5c88
César Rodrigues <cesar.rodrigues@mindera.com>
2022-07-18 14:08:48 +0100

    Update with gcloud commands (using now Google credentials for authentication)

--- setup.sh
+++
@@ -1,6 +0,0 @@
-kubectl config set-cluster workshop --embed-certs --certificate-authority=workshop.ca.crt --server=https://34.65.48.127
-kubectl config set-credentials workshop --auth-provider=gcp
-export GOOGLE_APPLICATION_CREDENTIALS=workshop.json
-kubectl config set-context workshop --cluster=workshop --user=workshop --namespace=$1
-kubectl config use-context workshop
-kubectl get nodes
